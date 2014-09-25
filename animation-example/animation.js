e25db92ce742e846466945a5e6f85a67683d4ead
sfletche <sfletche@gmail.com>
2014-09-24 21:50:05 -0700

    adding location output with fade in transition

--- animation-example/animation.js
+++ animation-example/animation.js
@@ -17,27 +17,31 @@ app.saveLocation = function() {
 
 app.displayAddedLocation = function(location_name) {
     var div = document.createElement("div");
+    div.style.opacity = 0;
+    div.className = "single-location";
     div.appendChild(document.createTextNode(location_name));
-    document.getElementById('recorded-locations').appendChild(div);
+    document.getElementById('recorded-locations').appendChild(div); 
+    window.getComputedStyle(div).opacity;   
+    div.style.opacity = 1;
 };
 
 app.assignTestLocations = function() {
     app.locations = [
         {
-            'location': 'Arbor Lodge',
+            'location_name': 'Arbor Lodge',
             'center': [-13657899.462245308, 5712246.434680445],
             'zoom': 16
         },
         {
-            'location': 'West Coast',
+            'location_name': 'West Coast',
             'center': [-13548022.56416822, 4514521.121707927],
             'zoom': 10
         }, {
-            'location': 'East Coast',
+            'location_name': 'East Coast',
             'center': [-8468032.5702836, 4689460.537542358],
             'zoom': 9
         }, {
-            'location': 'MidWest',
+            'location_name': 'MidWest',
             'center': [-10468032.5702836, 4589460.537542358],
             'zoom': 8
         }
@@ -50,36 +54,46 @@ app.showTimeline = function() {
         app.assignTestLocations();
     }
     for (var index in app.locations) {
-        var location = app.locations[index];        
-        app.showAnimations(location, x);
+        var location = app.locations[index];  
+        if (x == 0) {
+            app.showAnimations(location, x, true);    
+        } else {
+            app.showAnimations(location, x);
+        }        
         x++;
     }
 };
 
-app.showAnimations = function(location, x) {
+app.showAnimations = function(location, x, noBounce) {
     setTimeout( function() {
-        app.showAnimation(location);
+        if (x == 0) {}
+        app.showAnimation(location, noBounce);
+        app.displayAddedLocation(location.location_name);
     }, 5000 * x);
 };
 
-app.showAnimation = function(location) {        
+app.showAnimation = function(location, noBounce) {        
     var view = app.map.getView();
     var duration = 4000;
     var start = +new Date();
     var pan = ol.animation.pan({
         duration: duration,
-        source: (view.getCenter()), // @type {ol.Coordinate} 
-        start: start
+        source: (view.getCenter())
     });
     var bounce = ol.animation.bounce({
         duration: duration,
-        resolution: 7000, //4 * view.getResolution(),
-        start: start
+        resolution: 7000
     });
     var zoom = ol.animation.zoom({
-        resolution: map.getView().getResolution()
+        resolution: map.getView().getResolution(),
+        duration: duration / 2
     });
-    app.map.beforeRender(pan, bounce, zoom);
+    if (noBounce) {
+        app.map.beforeRender(zoom);    
+    } else {
+        app.map.beforeRender(pan, bounce, zoom);
+    }
+    
     view.setCenter(location.center);  
     view.setZoom(location.zoom);
 };
