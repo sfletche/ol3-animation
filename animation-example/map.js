12363eac030033a7d5eb9012471eb392abf3cb5a
sfletche <sfletche@gmail.com>
2014-09-27 14:27:57 -0700

    initial markers commit

--- animation-example/map.js
+++ animation-example/map.js
@@ -1,3 +1,4 @@
+app = {};
 
 var london = ol.proj.transform([-0.12755, 51.507222], 'EPSG:4326', 'EPSG:3857');
 var bern = ol.proj.transform([7.4458, 46.95], 'EPSG:4326', 'EPSG:3857');
@@ -10,12 +11,29 @@ var view = new ol.View({
   zoom: 3
 });
 
+var markers = new ol.layer.Vector({
+  source: new ol.source.Vector({
+    features: [
+      // new ol.Feature({
+      //   geometry: new ol.geom.Point(usa),
+      //   name: 'USA'
+      // })
+    ]
+  }),
+  style: new ol.style.Style({
+    image: new ol.style.Icon({
+      src: '../images/home_marker_icon_resized.png'
+    })
+  })
+});
+
 var map = new ol.Map({
   layers: [
     new ol.layer.Tile({
       preload: 4,
       source: new ol.source.OSM()
-    })
+    }),
+    markers,
   ],
   renderer: exampleNS.getRendererFromQueryString(),
   target: 'map',
@@ -27,8 +45,12 @@ var map = new ol.Map({
   view: view
 });
 
-app = {};
+map.on('show', function() {
+  alert('show!');
+});
+
 app.map = map;
+app.markers = markers;
 
 function onMoveEnd(evt) {
   var map = evt.map;
